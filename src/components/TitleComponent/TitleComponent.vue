74797ef7fd5e68089e933622463ceb18f74ed9fa
Gio Justiniano <giomikee.20@gmail.com>
2025-07-12 21:42:35 +0200

    feat(Icon): Added alt text

--- src/components/TitleComponent/TitleComponent.vue
+++ src/components/TitleComponent/TitleComponent.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container class="d-flex justify-center align-center ga-1">
-    <v-img height="50" max-width="50" src="/star-wars-logo.svg" />
+    <v-img
+      alt="Star wars logo"
+      height="50"
+      max-width="50"
+      src="/star-wars-logo.svg"
+    />
     <h1 class="d-md-none d-sm-block text-h4">Star Wars Explorer</h1>
     <h1 class="d-none d-md-block text-h2">Star Wars Explorer</h1>
   </v-container>
